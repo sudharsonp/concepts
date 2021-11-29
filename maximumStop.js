4beebd7185b2bb32a7550762e52bcd7ef8c5560e
Sudharson Pandiarajan <sudharson.p@gmail.com>
2021-11-29 14:31:41 +0530

    max  stop tains

--- maximumStop.js
+++ maximumStop.js
@@ -1,6 +1,6 @@
 const arrivals = [1000, 1010, 1000, 1030,1200,900]
 const departure = [1030,1030,1020,1230,1230,1005]
-const platforms = [ 1,1, 2,2,3,1]
+const platforms = [1,1,2,2,3,1]
 
 const group = arrivals.reduce((acc, val, i)=>{
     const pl = platforms[i]
@@ -16,3 +16,35 @@ const group = arrivals.reduce((acc, val, i)=>{
 }, {})
 
 console.log(group)
+
+const computePlatformNeeded = (arrivals, departures) =>{
+    let platformsNeeded = 0
+    let i = 1
+    let j = 0
+    let result = 1
+    while(i < arrivals.length && j < departures.length){
+        if(arrivals[i] < departures[j]){
+            platformsNeeded++
+            i++
+        }else if (arrivals[i] > departures[j]) {
+            j++
+        }
+
+        if(platformsNeeded > result){
+            result = platformsNeeded
+        }
+    }
+    return result
+}
+
+const maxTrainstopped = Object.keys(group).reduce((stop, key) => {
+    const value = group[key]
+    const platFormNeeds = computePlatformNeeded(value.arr, value.dep)
+    const totalTrains = value.arr.length
+    const maximumStopableInPlatform =  totalTrains - platFormNeeds + 1
+    stop = stop + maximumStopableInPlatform
+    return stop
+}, 0 )
+
+console.log('maxTrainstopped',maxTrainstopped)
+
